17853c0ef16a51a5e0451fd1a0f0d38461cf722c
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-15 20:21:29 -0300

    fix: .env place

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv/config');
+require('dotenv').config();
 
 const { HOST, PASSWORD_POSTGRES, DATABASE, DB_USERNAME, DB_PORT } = process.env;
 
--- src/database/models/patients.js
+++ src/database/models/patients.js
@@ -1,5 +1,6 @@
 const Patients = (sequelize, DataTypes) => {
   const patient = sequelize.define("Patients", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: DataTypes.STRING,
     date_of_birth: DataTypes.DATE,
     email: DataTypes.STRING,
