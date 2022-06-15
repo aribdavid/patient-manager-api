f149534effd5be1d73741728485f17939d50f079
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-15 18:40:32 -0300

    feat: created MVC architecture and create entrypoint of api

---
+++ src/api.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors');
+const  bodyParser = require("body-parser")
+
+// const { patients } = require('./database/models');
+
+
+const app = express()
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const port = process.env.PORT 
+
+app.use(express.json());
+
+// app.get('/', async (_req, res) => {
+
+
+//   const getPatients = await patients.findAll();
+
+//   return res.status(201).json(getPatients);
+// });
+
+app.listen(port, () => console.log(`Servidor online na porta ${port}`));
