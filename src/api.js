836a26f5b22f379d066aff8741c02eea33ef1ea1
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-15 20:54:25 -0300

    removed error middleware

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const errorHandler = require('./middlewares/errorHandler');
+// const errorHandler = require('./middlewares/errorHandler');
 const router = require('./routers');
 
 const app = express();
@@ -14,6 +14,6 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/', router.patient);
-app.use(errorHandler);
+// app.use(errorHandler);
 
 app.listen(port, () => console.log(`Servidor online na porta ${port}`));
