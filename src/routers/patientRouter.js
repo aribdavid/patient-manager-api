34c5de7c5c1ccd4711e51defa57a2efd6fbdadef
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-06-17 13:03:39 -0300

    feat: created updatePatient route

--- src/routers/patientRouter.js
+++ src/routers/patientRouter.js
@@ -6,6 +6,7 @@ const patientController = require('../controllers/patientController');
 const router = express.Router();
 
 router.get('/patient', rescue(patientController.getAll));
+router.put('/patient/:id', rescue(patientController.updatePatient));
 router.post('/patient', rescue(patientController.createPatient));
 router.get('/patient/:id', rescue(patientController.getById));
 router.delete('/patient/me', rescue(patientController.deletePatient));
